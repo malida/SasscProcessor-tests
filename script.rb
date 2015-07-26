9641dc9fa3c5b19288c40e986da491749bfdec19
Guillermo Iguaran <guilleiguaran@gmail.com>
2015-07-26 13:04:06 -0500

    moar fixes

--- script.rb
+++ script.rb
@@ -1,9 +1,10 @@
 require 'sprockets'
+require 'sprockets/sassc_processor'
 
 project_root = File.expand_path(File.dirname(__FILE__))
 assets = Sprockets::Environment.new(project_root) do |env|
   env.logger = Logger.new(STDOUT)
-  env.register_transformer 'text/sass', 'text/css', Sprockets::SassCProcessor.new
+  env.register_transformer 'text/sass', 'text/css', Sprockets::SasscProcessor.new
 end
 
 assets.append_path(File.join(project_root, 'stylesheets'))
