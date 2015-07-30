4d43a073acf6b2dc58830b39ace689f7e66020e7
malida <malida310892@gmail.com>
2015-07-29 20:19:34 -0500

    test ScsscProcessor

--- script.rb
+++ script.rb
@@ -5,8 +5,12 @@ project_root = File.expand_path(File.dirname(__FILE__))
 assets = Sprockets::Environment.new(project_root) do |env|
   env.logger = Logger.new(STDOUT)
   env.register_transformer 'text/sass', 'text/css', Sprockets::SasscProcessor.new
+  env.register_transformer 'text/scss', 'text/css', Sprockets::ScsscProcessor.new
 end
 
 assets.append_path(File.join(project_root, 'stylesheets'))
 
-puts assets.find_asset('base.css')
+#puts assets.find_asset('application.css')
+#puts assets.find_asset('reset.css')
+#puts assets.find_asset('base.css')
+puts assets.find_asset('base2.css')
